51337815ee836eec16a89d91454d3dc6a2866bc4
Awais-Arif <arifawais328@gmail.com>
2022-08-01 14:40:29 +0500

    final-cut

--- src/App.js
+++ src/App.js
@@ -1,24 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from "react";
+import "./index.css";
+import Header from "./Header";
+import Figure from "./Figure";
+import WrongLetters from "./WrongLetters";
+import Word from "./Word";
+import PopUp from "./PopUp";
+import Notification from "./Notification";
+import {showNotification as show} from "./Helper";
+import {checkWin} from "./Helper";
+
+const words = ["application", "programing", "interface", "wizard"];
+let selectedWord = words[Math.floor(Math.random() * words.length)];
 
 function App() {
+  const [playable, setPlayable] = useState(true);
+  const [correctLetters, setCorrectLetters] = useState([]);
+  const [wrongLetters, setWrongLetters] = useState([]);
+  const [showNotification, setshowNotification] = useState(false);
+
+  useEffect(() => {
+    const handleKeyDown = (event) => {
+      const {key, keyCode} = event;
+      if (playable && keyCode >= 65 && keyCode <= 90) {
+        const letter = key.toLowerCase();
+
+        if (selectedWord.includes(letter)) {
+          if (!correctLetters.includes(letter)) {
+            setCorrectLetters((currentLetters) => [...currentLetters, letter]);
+          } else {
+            show(setshowNotification);
+          }
+        } else {
+          if (!wrongLetters.includes(letter)) {
+            setWrongLetters((wrongLetters) => [...wrongLetters, letter]);
+          } else {
+            show(setshowNotification);
+          }
+        }
+      }
+    };
+    window.addEventListener("keydown", handleKeyDown);
+    return () => window.removeEventListener("keydown", handleKeyDown);
+  }, [wrongLetters, correctLetters, playable]);
+
+  function playAgain() {
+    setCorrectLetters([]);
+    setWrongLetters([]);
+    const random = Math.floor(Math.random() * words.length);
+    selectedWord = words[random];
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      <div className="game-container">
+        <Figure wrongLetters={wrongLetters} />
+        <WrongLetters wrongLetters={wrongLetters} />
+        <Word selectedWord={selectedWord} correctLetters={correctLetters} />
+      </div>
+      <PopUp
+        correctLetters={correctLetters}
+        wrongLetters={wrongLetters}
+        selectedWord={selectedWord}
+        setPlayable={setPlayable}
+        playAgain={playAgain}
+      />
+      <Notification showNotification={showNotification} />
+    </>
   );
 }
 
